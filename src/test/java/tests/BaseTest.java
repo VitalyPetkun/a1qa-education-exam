49aa2917230b90e8e9a85aabd227a978ec09db66
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-03-25 22:09:46 +0300

    Add file BaseTest.java

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import aquality.selenium.browser.AqualityServices;
+import org.testng.annotations.*;
+import services.Uri;
+import utils.*;
+import utils.api.APIUtils;
+
+public class BaseTest {
+
+    @BeforeMethod
+    public void setup() {
+        SmartLogger.logInfo("Set base uri");
+        APIUtils.setupBaseUri(PropertiesManager.getConfigValue(Uri.API_URI.getUri()));
+        SmartLogger.logInfo("Set window size maximize");
+        AqualityServices.getBrowser().getDriver().manage().window().maximize();
+        SmartLogger.logInfo("Timeout load browser");
+        AqualityServices.getBrowser().getDriver().manage().timeouts();
+    }
+
+    @AfterMethod
+    protected void quitDriver() {
+        SmartLogger.logInfo("Quit browser");
+        AqualityServices.getBrowser().getDriver().quit();
+    }
+}
