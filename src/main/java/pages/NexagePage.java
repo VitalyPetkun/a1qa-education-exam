ca492370f7e5695c1e3d1d042012797ae7000c16
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-03-27 05:46:44 +0300

    Delete file XmlConverter.java

--- src/main/java/pages/NexagePage.java
+++ src/main/java/pages/NexagePage.java
@@ -5,14 +5,15 @@ import aquality.selenium.elements.interfaces.ILabel;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NexagePage extends Form {
 
-    private final List<ILabel> tests = getElementFactory().findElements(By.xpath("//table[@class='table']//tr"), ElementType.LABEL);
-
     private final ILabel currentTestsPage = getElementFactory().getLabel(By.xpath("//ul[@class='pagination']//li[@class='active']//a"),
             "Current tests page");
+
+    private List<ILabel> tests = new ArrayList<>();
 
     private final String TD_ATTRIBUTE = ("td");
     private final String TEST_NAME_ATTRIBUTE = String.format("//%s[1]//a", TD_ATTRIBUTE);
@@ -26,32 +27,39 @@ public class NexagePage extends Form {
         super(By.xpath("//ol[@class='breadcrumb']//li[text()='Nexage']"), "Nexage page");
     }
 
+    private List<ILabel> getTests() {
+        if (tests.size() == 0)
+            tests = getElementFactory().findElements(By.xpath("//table[@class='table']//tr"), ElementType.LABEL);
+
+        return tests;
+    }
+
     public int getSizeTests() {
-        return tests.size();
+        return getTests().size();
     }
 
     public String getTestName(int index) {
-        return tests.get(index).findChildElement(By.xpath(TEST_NAME_ATTRIBUTE), ElementType.LABEL).getText();
+        return getTests().get(index).findChildElement(By.xpath(TEST_NAME_ATTRIBUTE), ElementType.LABEL).getText();
     }
 
     public String getMethodName(int index) {
-        return tests.get(index).findChildElement(By.xpath(METHOD_NAME_ATTRIBUTE), ElementType.LABEL).getText();
+        return getTests().get(index).findChildElement(By.xpath(METHOD_NAME_ATTRIBUTE), ElementType.LABEL).getText();
     }
 
     public String getStatus(int index) {
-        return tests.get(index).findChildElement(By.xpath(STATUS_ATTRIBUTE), ElementType.LABEL).getText();
+        return getTests().get(index).findChildElement(By.xpath(STATUS_ATTRIBUTE), ElementType.LABEL).getText();
     }
 
     public String getStartTime(int index) {
-        return tests.get(index).findChildElement(By.xpath(START_TIME_ATTRIBUTE), ElementType.LABEL).getText();
+        return getTests().get(index).findChildElement(By.xpath(START_TIME_ATTRIBUTE), ElementType.LABEL).getText();
     }
 
     public String getEndTime(int index) {
-        return tests.get(index).findChildElement(By.xpath(END_TIME_ATTRIBUTE), ElementType.LABEL).getText();
+        return getTests().get(index).findChildElement(By.xpath(END_TIME_ATTRIBUTE), ElementType.LABEL).getText();
     }
 
     public String getDuration(int index) {
-        return tests.get(index).findChildElement(By.xpath(DURATION_ATTRIBUTE), ElementType.LABEL).getText();
+        return getTests().get(index).findChildElement(By.xpath(DURATION_ATTRIBUTE), ElementType.LABEL).getText();
     }
 
     public String getCurrentTestsPage() {
